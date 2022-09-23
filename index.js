47b792ed29ff17a74282fd6bc04422dade8d7ca1
Jia-Yi Jian <jyjian@illinois.edu>
2022-09-22 21:04:58 -0700

    complete

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 var image2 = document.querySelectorAll("img")[1];
 
 image2.setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+
+if (randomNumber1 === randomNumber2) {
+  document.querySelector("h1").innerHTML = "Draw";
+} else if (randomNumber1 > randomNumber2) {
+  document.querySelector("h1").innerHTML = "🚩Player 1 Wins!";
+} else {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!🚩";
+}
