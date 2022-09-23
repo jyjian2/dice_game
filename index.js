3bf30be5f91be6774ac91142e3e22eea2a63a54c
Jia-Yi Jian <jyjian@illinois.edu>
2022-09-22 20:37:52 -0700

    set random dice number for both

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-var randomNumner1 = Math.floor(math.random() * 6) + 1;
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", "images/dice" + randomNumber2 + ".png");
